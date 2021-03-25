1abb3b97d2a42624afa2a7a9daa889c674d458d7
rkdwldk24 <rkdwldk16@gmail.com>
2021-03-26 07:51:32 +0900

    commit portpolio and don't remove

--- portpolio/main.js
+++ portpolio/main.js
@@ -1,38 +1,24 @@
 "use strict";
 
-/*nav*/
-//반응형 메뉴 만들기
+//naviber
 const toggleBtn = document.querySelector(".navbar_toggleBtn");
 const menu = document.querySelector(".main_menu");
 const icons = document.querySelector(".icons");
+const content = document.querySelector(".content");
+const footer = document.querySelector("footer");
+const footerInner = document.querySelector(".footer__inner");
 
-//manu
-const menuIntroduction = document.querySelector(".menu_Introduction");
-const document_first = document.querySelector(".document_first");
-const menuSkills = document.querySelector(".menu_Skills");
-const document_second = document.querySelector(".document_second");
-const menuProjects = document.querySelector(".menu_Projects");
-const projects = document.querySelector(".projects");
-const menuFooter = document.querySelector(".menu_footer");
-const container = document.querySelector(".container");
 
-/*nav*/
-//마우스 이벤트 함수를 통해서 active상태에서 실행할 토글 함수 만들어주기
+//naviber
 toggleBtn.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  icons.classList.toggle("active");
-}); //toggleBtn이 click될때마다 active 클래스를 토글할 것
+    menu.classList.toggle("active");
+    icons.classList.toggle("active");
 
-//메뉴를 누르면 해당 페이지로 이동
-menuIntroduction.addEventListener("click", () => {
-  document_first.scrollIntoView({ behavior: "smooth", block: "start" });
+    content.style.display = 'none';
+    footer.style.position = 'absolute';
+    footer.style.top = '400px';
+    footerInner.style.margin = 'auto';
+    footerInner.style.textAlign = 'center';
 });
-menuSkills.addEventListener("click", () => {
-  document_second.scrollIntoView({ behavior: "smooth", block: "start" });
-});
-menuProjects.addEventListener("click", () => {
-  projects.scrollIntoView({ behavior: "smooth", block: "start" });
-});
-menuFooter.addEventListener("click", () => {
-  container.scrollIntoView({ behavior: "smooth", block: "start" });
-});
+
+
